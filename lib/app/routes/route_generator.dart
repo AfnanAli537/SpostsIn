6818b13e74103e31eaeb2534e18f8d399e053119
Afnan Ali <afnan.alial537@gmail.com>
2025-10-24 21:14:19 +0300

    splash screen done

--- lib/app/routes/route_generator.dart
+++ lib/app/routes/route_generator.dart
@@ -2,12 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sports_in/app/routes/app_routes.dart';
 import 'package:sports_in/view/auth/login/presentation/login_screen.dart';
+import 'package:sports_in/view/onboarding/presentation/onboarding_screen.dart';
+import 'package:sports_in/view/onboarding/presentation/privacy_policy_screen.dart';
 
 abstract class RoutesManager{
   static Route? router(RouteSettings settings){
     switch(settings.name){
       case AppRoutes.login:{
         return CupertinoPageRoute(builder: (context)=> LoginScreen());
+      }
+      case AppRoutes.privacyPolicy:{
+        return CupertinoPageRoute(builder: (context)=> PrivacyPolicyScreen());
+      }
+      case AppRoutes.onboarding:{
+        return CupertinoPageRoute(builder: (context)=> OnboardingScreen());
       }
     }
     return null;
--- lib/core/constants/strings_manager.dart
+++ lib/core/constants/strings_manager.dart
@@ -1,1 +1,18 @@
-class StringsManager {}
+class StringsManager {
+  ///onboarding 
+  ///title
+  static const String onboarding1Title = "Discover Sports Talents";
+  static const String onboarding2Title = "Post Opportunities";
+  static const String onboarding3Title = "Build Your Sports Profile";
+  static const String onboarding4Title = "AI Video Analysis";
+  static const String onboarding5Title = "Connect & Communicate";
+  ///description
+  static const String onboarding1desc="Find and connect with top athletes through AI-powered talent discovery. Filter by sport, skills, and achievements.";
+  static const String onboarding2desc="Create and publish tryouts, competitions, or sponsorship offers to attract the right athletes";
+  static const String onboarding3desc="Join SportsIn and take your sports career to the next level.";
+  static const String onboarding4desc="Upload your performance videos and get instant AI-powered analysis on your skills, movements, and progress";
+  static const String onboarding5desc="Chat directly with coaches, clubs, and athletes. Build your sports network and stay updated with new opportunities.";
+
+
+
+}
